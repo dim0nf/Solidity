47fe391631b96d402c26d64d092eeb742abf9f7e
dim0nf <70643782+dim0nf@users.noreply.github.com>
2023-02-26 18:25:49 +0400

    Update Donation.sol

--- Task_01/Donation.sol
+++ Task_01/Donation.sol
@@ -23,8 +23,20 @@ contract Donation is IDonation{
         require(msg.sender == owner, "you are not an owner!");
         if (_addr == address(0)) _addr = owner;
         if (_amount == 0 ) _amount = address(this).balance;
+        if (_amount > address(this).balance ) _amount = address(this).balance;
         (bool success,) = _addr.call{value: _amount}(new bytes(0));
         require(success, "failed to withdraw");
         emit Withdrawall(_addr, _amount);
+    }
+
+    function withdrawMembers (uint256 _amount) external {
+        require(address(this).balance > 0, "contact zero balance");
+        address payable _addr = payable(msg.sender);
+        if (_amount  > payments[msg.sender]) _amount = payments[msg.sender];
+        if (_amount > address(this).balance) _amount = address(this).balance;
+        payments[msg.sender] -= _amount;
+        (bool success,) = _addr.call{value: _amount}(new bytes(0));
+        require(success, "failed to withdraw");
+        emit WithdrawMembers(msg.sender, _amount);
     }
 }
